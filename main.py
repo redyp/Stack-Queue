1155aea7c02a8dae65654f776c2ab430b50516c4
redyp <redy.p@students.amikom.ac.id>
2022-01-23 15:21:45 +0700

    Add Push and Pop

--- main.py
+++ main.py
@@ -28,3 +28,23 @@ class Stack:
             temp = temp._next
         temp_str += 'None'
         return temp_str
+    
+    def push(self, value: int):
+        push_node = Node(value)
+        if self.head == None:
+            self.head = push_node
+        else:
+            push_node._next = self.head
+            self.head = push_node
+        self.lenght += 1
+        return True
+    
+    def pop(self) -> Node:
+        if self.lenght == 0:
+            return None
+        temp = self.head
+        self.head = temp._next
+        temp._next = None
+        self.lenght -= 1
+        return temp
+
