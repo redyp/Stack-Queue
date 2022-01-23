1a148d639dc3f673a93a249d1cdc66ec243be2b2
redyp <redy.p@students.amikom.ac.id>
2022-01-23 19:44:45 +0700

    Add Queue

--- main.py
+++ main.py
@@ -48,3 +48,27 @@ class Stack:
         self.lenght -= 1
         return temp
 
+@dataclass
+class Queue:
+    value: int = field(repr=False, default=None)
+    head: Node = field(init=False, default=None)
+    tail: Node = field(init=False, default=None)
+    lenght: int = field(init=False, default=0)
+
+    def __post_init__(self):
+        if self.value is not None:
+            new_node = Node(self.value)
+            self.head = new_node
+            self.tail = new_node
+            self.lenght = 1
+    
+    def __str__(self) -> str:
+        if self.head == None:
+            return 'Queue Kosong'
+        temp = self.head
+        temp_str = ''
+        while temp:
+            temp_str += f'{temp.value} -> '
+            temp = temp._next
+        temp_str += 'None'
+        return temp_str
